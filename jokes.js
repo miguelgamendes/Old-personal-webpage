864ae4d805c2c02ff1aa5a46d9ef1dc7c33a3f41
Miguel Mendes <zephiro309@gmail.com>
2017-10-30 18:13:09 +0000

    Joke site addition.

---
+++ jokes.js
@@ -0,0 +1,25 @@
+$(document).ready( function () {
+    jokeUpdate();
+    progressBar();
+
+    window.setInterval(jokeUpdate, 10000);
+    window.setInterval(progressBar, 10000);
+});
+
+function jokeUpdate() {
+    $.getJSON('http://api.icndb.com/jokes/random', function(data) {
+        if(data["type"] == "success")
+            $('#joke').replaceWith("<h1 id=\"joke\" class=\"text-right box_textshadow\">" + data["value"]["joke"] + "</h1>");
+        else
+            $('#joke').replaceWith("<h1 id=\"joke\">" + "Even Chuck Norris can't retrieve jokes from an API that doesn't work right..." + "</h1>");
+    });
+}
+
+function progressBar() {
+    var progress = $('.progress');
+    var progressBar = $('.progress-bar');
+
+    progressBar.stop();
+    progressBar.animate({width: "0%"}, 1000);
+    progressBar.animate({width: "100%"}, 10000);
+}
