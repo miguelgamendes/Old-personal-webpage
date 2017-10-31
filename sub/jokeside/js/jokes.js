2f46d078ad18b81374715620db09edcea6f3bf89
Miguel Mendes <zephiro309@gmail.com>
2017-10-31 16:23:18 +0000

    Added more appropriate images, restructured project structure for separation.

--- sub/jokeside/js/jokes.js
+++ sub/jokeside/js/jokes.js
@@ -13,8 +13,8 @@ function jokeUpdate() {
         else
             $('#joke').replaceWith("<h1 id=\"joke\">" + "Even Chuck Norris can't retrieve jokes from an API that doesn't work right..." + "</h1>");
 
-        var fileNumber = Math.floor(Math.random() * 5);
-        $('.full-width-image').css("background", "url(\"../images/chucknorris/" + fileNumber + ".jpg\") no-repeat center center fixed");
+        var fileNumber = Math.floor(Math.random() * 11);
+        $('.full-width-image').css("background", "url(\"images/" + fileNumber + ".jpg\") no-repeat center center fixed");
     });
 }
 
