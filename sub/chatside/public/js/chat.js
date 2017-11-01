806a3df33473b29830e27679e1b4dd52d062ec8f
Miguel Mendes <zephiro309@gmail.com>
2017-11-01 21:54:04 +0000

    Added a few comments for clarification.

--- sub/chatside/public/js/chat.js
+++ sub/chatside/public/js/chat.js
@@ -1,10 +1,16 @@
 var username;
 
+/*
+* Shows the username prompt modal after page load
+*/
 $(window).on('load',function(){
     $('#initialModal').modal({backdrop: 'static', keyboard: false});    
     $('#initialModal').modal('show');
 });
 
+/*
+* Sets username
+*/
 function setUsername() {
     if($("#username").val() != "") {
         username = $("#username").val();
@@ -12,17 +18,25 @@ function setUsername() {
     }
 }
 
-//listen for database changes
+/*
+* Listens to database changes, waiting for chat updates
+*/
 firebase.database().ref('messages/').on('child_added', function(snapshot) {
     console.log("RECEIVED MESSAGE: " + snapshot.val().text);
     receiveMessage(snapshot.val().author, snapshot.val().text);
 })
 
+/*
+* Adds timestamp to message
+*/
 function sendMessage(message) {
     var timeStamp = Math.floor(Date.now());
     logMessage(message, timeStamp);
 }
 
+/*
+* Sends message to database
+*/
 function logMessage(message, timestamp) {
     firebase.database().ref('messages/' + timestamp).set({
         author: username,
@@ -30,6 +44,9 @@ function logMessage(message, timestamp) {
     });
 }
 
+/*
+* Updates displayed messages to include any new messages on the database
+*/
 function receiveMessage(author, message) {
     //add received message to list of displayed messages
     $("#chat").append("<div class=\"alert alert-success\" role=\"alert\"><b style=\"float: left\">" + author + ":</b><p>&nbsp" + message + "</p></div>");
@@ -38,6 +55,9 @@ function receiveMessage(author, message) {
     document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
 }
 
+/*
+* Awaits the 'Enter' key in the user text input field
+*/
 function onInputChange() {
     var key = window.event.keyCode;
 
@@ -50,6 +70,9 @@ function onInputChange() {
     }
 }
 
+/*
+* Prepares text area content to be sent and clears it
+*/
 function onClickSend() {
     message = document.getElementById("inputmessage").value;
     if(message != "")
